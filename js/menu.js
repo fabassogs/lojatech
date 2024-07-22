428af9f0519335ffa17866d562b8cfeb59495e02
fabassogs <fabricio.aacruz@senacsp.edu.br>
2024-07-22 16:59:19 -0300

    Iniciando o projeto com JavaScript

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('#nav ul');
+const hamburguer = document.getElementById('hamburguer');
+
+hamburguer.addEventListener('click', ()=>{
+    nav.classList.toggle('toggle');
+})
