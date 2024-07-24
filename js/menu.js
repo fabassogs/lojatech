cfa741c5e7c952d56325b84b079fc3bb2b49d875
fabassogs <fabricio.aacruz@senacsp.edu.br>
2024-07-24 17:04:28 -0300

    Altertação do Footer

--- js/menu.js
+++ js/menu.js
@@ -2,5 +2,10 @@ const nav = document.querySelector('#nav ul');
 const hamburguer = document.getElementById('hamburguer');
 
 hamburguer.addEventListener('click', ()=>{
-    nav.classList.toggle('toggle');
+    nav.classList.toggle('toggle'); //
+    if(nav.className == 'toggle') {
+        hamburguer.innerHTML = '<i class="fa fa-close"></1>'
+    } else {
+        hamburguer.innerHTML = '<i class="fa fa-bars"></i>'
+    }
 })
